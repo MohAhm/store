842bee4bd35b974ad8b402b94db0b6544b269aab
Mohamed Ali <a.mohamed.cali@gmail.com>
2025-06-30 22:05:32 +0200

    Initial setup

---
+++ src/test/java/com/moxamed/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moxamed.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
