3ef9b50d1c16d0f1f5f6a3721e0033d1cb56f1d2
Mohamed Ali <a.mohamed.cali@gmail.com>
2025-07-02 21:56:46 +0200

    first Spring controller

--- src/main/java/com/moxamed/store/HomeController.java
+++ src/main/java/com/moxamed/store/HomeController.java
@@ -1,12 +1,17 @@
 package com.moxamed.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/") // root
     public String index() {
+        System.out.println("appName: " + appName);
         return "index.html";
     }
 }
