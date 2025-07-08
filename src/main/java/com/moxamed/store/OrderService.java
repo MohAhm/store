a498e070e5e85d31b1bf7c4207e96a825fe3b807
Mohamed Ali <a.mohamed.cali@gmail.com>
2025-07-08 20:38:01 +0200

    Add Bean selection

--- src/main/java/com/moxamed/store/OrderService.java
+++ src/main/java/com/moxamed/store/OrderService.java
@@ -1,9 +1,19 @@
 package com.moxamed.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
+//    @Autowired
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
